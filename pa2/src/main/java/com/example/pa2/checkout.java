ce9fa03adb587ce10850f848682c845d1256adc2
78redok <81646320+78redok@users.noreply.github.com>
2022-05-04 04:10:38 -0700

    imlosingmymind

--- pa2/src/main/java/com/example/pa2/checkout.java
+++ pa2/src/main/java/com/example/pa2/checkout.java
@@ -7,21 +7,207 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.http.HttpSession;
 import java.util.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 
 @WebServlet("/checkout")
 public class checkout extends HttpServlet
 {
+    
+
     // process "get" requests from clients
-    protected void doGet(HttpServletRequest request,
+    protected void processRequest(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException
     {
-        response.setContentType("text/html");
+
+        response.setContentType("text/html;charset=UTF-8");
+        String gameId = request.getParameter("gameId");
+
+        HttpSession session = request.getSession(true);
+        HashMap<String, Integer> cart = (HashMap<String, Integer>) session.getAttribute("cart");
+
+        if (cart == null) {
+            cart = new HashMap<String, Integer>();
+            cart.put(gameId, 1);
+        } else             {
+            Integer quantity = cart.get(gameId);
+
+            if(quantity == null)
+                cart.put(gameId, 1);
+            else
+                cart.put(gameId, ++quantity);
+        }
+
+        session.setAttribute("cart", cart);
+
         PrintWriter out = response.getWriter();
 
-        // send HTML page to client
-        out.println("<html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <link rel = \"stylesheet\" href = \"style.css\" type=\"text/css\"> <link rel = \"stylesheet\" href= \"https://pro.fontawesome.com/releases/v5.10.0/css/all.css\" type=\"text/css\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Tenzo and Richard's Store</title> <script src=\"https://code.jquery.com/jquery-3.6.0.js\"></script> <script src=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js\"></script> <script> $(document).ready(function(){ $(\'#hamburger\').click(function(){ $(\'ul\').toggleClass(\'show\'); }); }); </script> <link rel=\"icon\" href=\"https://www.nicepng.com/png/full/96-961724_svg-transparent-download-dog-s-by-seng-hoong.png\"></head><body> <header> <nav> <img src=\"mainlogo.png\" alt=\"logo\"> <ul> <li><a href = \"home\">Home</a></li> <li><a href = \"about\">About Our Company</a></li> <li><a href = \"products\">Products</a></li> <li class = \"current\"><a href = \"checkout\">Check Out</a></li> </ul> <label id=\"hamburger\"> <i class=\"fas fa-bars\"></i> </label> </nav> </header>");
-        out.println(" <h1>Shopping Cart Implementation Coming Soon!</h1> <p1>(Outside the scope of this assignment)</p1></body></html>");
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "game_db", "root", "Mal3k$aM");
+            Statement stmt = con.createStatement();
+            double totalprice = 0;
+            int qty = 0;
+            out.println("<html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <link rel = \"stylesheet\" href = \"style.css\"> <link rel = \"stylesheet\" href= \"https://pro.fontawesome.com/releases/v5.10.0/css/all.css\"><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Tenzo and Richard's Store</title> <script src=\"https://code.jquery.com/jquery-3.6.0.js\"></script> <script src=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js\"></script> <script> $(document).ready(function(){ $('#hamburger').click(function(){ $('ul').toggleClass('show'); }); });"); 
+            
+            out.println("function myFunction(zipcode, name) {" +
+                "let a = zipcode.toString().length;" +
+                "if(a != 5) {" +
+                    "alert(\"Zip Code Must Be 5 Digits.\");" +
+                    "return false;}" +
+                "var regex = /^[a-zA-Z]+ [a-zA-Z]+$/;" +
+                "var b = name;" +
+                "if(!regex.test(name)){" +
+                    "alert('Invalid name given.');" +
+                    "return false;}" +
+                "}");
+            
+            out.println(" </script> <link rel=\"icon\" href=\"https://www.nicepng.com/png/full/96-961724_svg-transparent-download-dog-s-by-seng-hoong.png\"></head><body> <header> <nav> <img src=\"mainlogo.png\" alt=\"logo\"> <ul> <li><a href = \"home\">Home</a></li> <li><a href = \"about\">About Our Company</a></li> <li><a href = \"products\">Products</a></li> <li class = \"current\"><a href = \"checkout\">Check Out</a></li> </ul> <label id=\"hamburger\"> <i class=\"fas fa-bars\"></i> </label> </nav> </header>");
+            out.println("<div class=\"switchsection\">");
+            out.println("<div class=\"cards\">");
+            out.println("<div class=\"switch-products\">");
+            out.println("<h1>Cart Items</h1></div>");
+            out.println("<div class=\"card\">");
+            out.println("<div class=\"image-section\">");
+
+            for (String gameID : cart.keySet()) {
+                String sql = "SELECT * FROM games WHERE id =" + gameID;
+                ResultSet rs = stmt.executeQuery(sql);
+
+
+                while (rs.next()) {
+                totalprice += rs.getDouble("price");
+                qty++;
+                out.println("<table> <tr> <td>"+rs.getString("name")+"</td> <td style=\"text-align:right\">"+ rs.getDouble("price")+"</td> </tr></table>");
+                
+                }
+            }
+            out.println("<p1>" + totalprice + "</p1>");
+
+            out.println("<div class=\"overlay\" id=\"popupform\">" +
+            "<div class=\"wrapper\">" +
+                "<h2 id=\"productNameelden\">Check Out Form</h2>" +
+                "<br></br>");
+            out.println("<p1>Your items:</p1><br>");
+
+                for (String gameID : cart.keySet()) {
+                    String sql = "SELECT * FROM games WHERE id =" + gameID;
+                    ResultSet rs = stmt.executeQuery(sql);
+    
+    
+                    while (rs.next()) {
+                        out.println("<p1>" + rs.getString("name") + " Price: " + rs.getDouble("price") + "<br>");
+
+                    }
+                }
+
+            out.println("<br><p1>Total Price: " + totalprice + "</p1>" +
+                "<br></br>" +
+                "<a href=\"#\" class=\"close\">&times;</a>" +
+                "<div class=\"content\">" +
+                "<div class=\"container\">" +
+                "<form id=\"myForm\" name= \"formform\" onsubmit=\"return myFunction(document.getElementById('zipcode').value, document.getElementById('name').value)\" action=\"submitform\" method=\"GET\">" +
+                "</id>" +
+                "<input type=\"hidden\" name = \"quantity\" value="+qty+">" +
+                "<input type=\"hidden\" name = \"totprice\" value="+totalprice+">" +
+                "   <label>Full Name</label>" +
+                            "<input type=\"text\" name = \"full_name\" id = \"name\" placeholder = \"Your Full Name\" required>" +
+                            "<label>Phone Number</label>" +
+                            "<input type=\"text\" name = \"tele\" id = \"tele\" placeholder = \"Phone Number\" required>" +
+                            "<label>Shipping Address</label>" +
+                            "<input type=\"text\" name = \"shipper\" id = \"address\" placeholder = \"Address\" required>" +
+                            "<label>Shipping City</label>" +
+                            "<input type=\"text\" name = \"shipper_city\" placeholder = \"Address City\" required>" +
+                            "<label>Shipping State</label>" +
+                            "<input type=\"text\" name = \"shipper_state\" placeholder = \"Address State\" required>" +
+                            "<label>Zip Code</label>" +
+                            "<input type=\"text\" name = \"zipcode\" placeholder = \"Zip Code\" id = \"zipcode\" required>" +
+                            "<label>E-Mail Address</label>" +
+                            "<input type=\"email\" name = \"email\" id =\"email\" placeholder = \"Email Address\" required>" +
+                            "<label>Credit Card Name</label>" +
+                            "<input type=\"text\" name = \"credit_name\" id =\"credit_name2\" placeholder = \"Credit Card Name\" required>" +
+                            "<label>Card Number</label>" +
+                            "<input type=\"number\" name = \"credit_num\" placeholder = \"Credit Card Number\" required>" +
+                            "<label>Expiration Month</label>" +
+                            "<input type=\"number\" name = \"credit_month\" placeholder = \"Expiration Month\" required>" +
+                            "<label>Expiration Year</label>" +
+                            "<input type=\"number\" name = \"credit_year\" placeholder = \"Expiration Year\" required>" +
+                            "<label>Shipping Options</label><br><br>" +
+                            "<input type=\"radio\" id = \"Expedited\" name=\"opt_shipping\" value=\"Expedited Shipping\" required>" +
+                            "<label for =\"Expedited\">Expedited Shipping (1-3 days)</label><br>" +
+                            "<input type=\"radio\" id = \"Standard\" name=\"opt_shipping\" value=\"Standard Shipping\">" +
+                            "<label for =\"Standard\">Standard Shipping (5-7 days)</label><br><br><br>" +
+                            "<p1>Review Your Experience</p1>" +
+                            "<div class=\"container_rating\"> <div class=\"star-widget\"> <input type=\"radio\" name=\"rate\" id=\"rate-5\" value=1> <label for=\"rate-5\" class=\"fa fa-star\"></label> <input type=\"radio\" name=\"rate\" id=\"rate-4\" value=2> <label for=\"rate-4\" class=\"fa fa-star\"></label> <input type=\"radio\" name=\"rate\" id=\"rate-3\" value=3> <label for=\"rate-3\" class=\"fa fa-star\"></label> <input type=\"radio\" name=\"rate\" id=\"rate-2\" value=4> <label for=\"rate-2\" class=\"fa fa-star\"></label> <input type=\"radio\" name=\"rate\" id=\"rate-1\" value=5> <label for=\"rate-1\" class=\"fa fa-star\"></label><br><br> </div> </div>" +
+                            "<input type=\"submit\" value=\"Submit\"></form></div></div></div></div>");
+
+                        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+             //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+
+        out.println("<div class=\"button-group\"><a href=\"#popupform\" class=\"buy\">Review and Check Out</a></div>");
+
+ 
     }
+
+
+
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
--- pa2/src/main/java/com/example/pa2/nswitch.java
+++ pa2/src/main/java/com/example/pa2/nswitch.java
@@ -41,7 +41,7 @@ public class nswitch extends HttpServlet
 
         // send HTML page to client
         out.println("<html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <link rel = \"stylesheet\" href = \"style.css\"> <link rel = \"stylesheet\" href= \"https://pro.fontawesome.com/releases/v5.10.0/css/all.css\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Tenzo and Richard's Store</title> <script src=\"https://code.jquery.com/jquery-3.6.0.js\"></script> <script src=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js\"></script> <script> $(document).ready(function(){ $('#hamburger').click(function(){ $('ul').toggleClass('show'); }); });"); 
-        out.println(" </script> <link rel=\"icon\" href=\"https://www.nicepng.com/png/full/96-961724_svg-transparent-download-dog-s-by-seng-hoong.png\"></head><body> <header> <nav> <img src=\"mainlogo.png\" alt=\"logo\"> <ul> <li><a href = \"index\">Home</a></li> <li><a href = \"about\">About Our Company</a></li> <li class = \"current\"><a href = \"products\">Products</a></li> <li><a href = \"checkout\">Check Out</a></li> </ul> <label id=\"hamburger\"> <i class=\"fas fa-bars\"></i> </label> </nav> </header>");
+        out.println(" </script> <link rel=\"icon\" href=\"https://www.nicepng.com/png/full/96-961724_svg-transparent-download-dog-s-by-seng-hoong.png\"></head><body> <header> <nav> <img src=\"mainlogo.png\" alt=\"logo\"> <ul> <li><a href = \"home\">Home</a></li> <li><a href = \"about\">About Our Company</a></li> <li class = \"current\"><a href = \"products\">Products</a></li> <li><a href = \"checkout\">Check Out</a></li> </ul> <label id=\"hamburger\"> <i class=\"fas fa-bars\"></i> </label> </nav> </header>");
         out.println("<div class=\"switchsection\">");
         out.println("<div class=\"cards\">");
         out.println("<div class=\"switch-products\">");
@@ -49,21 +49,42 @@ public class nswitch extends HttpServlet
         while(rs.next()){                  
         out.println("<div class=\"card\">");
         out.println("<div class=\"image-section\">");
+            int gameId = rs.getInt("id"); 
             String imglocation = rs.getString("imgSrc");
             String prodname = rs.getString("name");
             String prodPrice = rs.getString("price");
             out.println("<img src=" + imglocation + "></div>");
+            out.println("<div class=\"description\">");
 //<a href="#popupkart">
 //<img src="./switch/kart.jpeg" alt="Mario Kart">
 //</a>
-            out.println("<div class=\"description\">");
 //"<h1>Mario Kart</h1>
 //<p><b>Price:</b><span>$49.99</span><del>$59.99</del></p></div>
+
+
+
             out.println("<h1>" + prodname + "</h1></div>");
             out.println("<p><b>Price: </b><span>" + prodPrice + "</span></p>");
-        out.println("<div class=\"button-group\">");
-        out.println("<a href=\"productdetails\" class=\"buy\">Product Details</a></div>");
-        out.println("</div>");
+        //out.println("<div class=\"button-group\">");
+        //out.println("<a href=\"productdetails\" class=\"buy\">Product Details</a></div>");
+        //out.println("</div>");
+
+        //<form action="/action_page.php" method="get">
+    //<label for="fname">First name:</label>
+    //<input type="text" id="fname" name="fname"><br><br>
+    //<label for="lname">Last name:</label>
+    //<input type="text" id="lname" name="lname"><br><br>
+     //<button type="submit">Submit</button>
+     //<button type="submit" formmethod="post">Submit using POST</button>
+       // </form>
+
+       out.println("<form  action=\"productdetails\" method=\"GET\">");
+       out.println("<input type=hidden id=gameId name=gameId value="+ gameId +">");
+       out.println("<input type=submit value=Details>");               
+       //out.println("<div class='button-group'><a href=\"productdetails\" class = 'buy' name = detailbutton value=" + gameId +">Product Details</button></div>");
+
+       out.println("</form></div>"); 
+
     }}
     catch(ClassNotFoundException e){
         e.printStackTrace();
